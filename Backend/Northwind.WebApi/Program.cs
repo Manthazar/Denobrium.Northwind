75432ff87f48ee1e46c9d76df070188d66338c04
Aron <arko@denobrium.net>
2023-11-21 21:34:42 +0100

    #3 Adding configuration unit and secret

--- Backend/Northwind.WebApi/Program.cs
+++ Backend/Northwind.WebApi/Program.cs
@@ -1,8 +1,13 @@
+using Northwind.Modules;
+
 var builder = WebApplication.CreateBuilder(args);
+var sqlConnectionString = builder.Configuration["Northwind:SqlConnectionString"];
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddRepositories(sqlConnectionString);
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
