2d551b5341f116d06ee183a835f42eecab0015ed
Aron <arko@denobrium.net>
2023-12-01 21:37:14 +0100

    #6 Remove nonsense null schema from snapshot to prevent pseudo code generation differences in git.

--- Backend/Northwind.Sql.Migrations/Migrations/NorthwindContextModelSnapshot.cs
+++ Backend/Northwind.Sql.Migrations/Migrations/NorthwindContextModelSnapshot.cs
@@ -48,7 +48,7 @@ namespace Northwind.Sql.Migrations.Migrations
 
                     b.HasIndex("TerritoryId");
 
-                    b.ToTable("EmployeeTerritories", (string)null);
+                    b.ToTable("EmployeeTerritories");
                 });
 
             modelBuilder.Entity("Northwind.Core.Models.Category", b =>
@@ -75,7 +75,7 @@ namespace Northwind.Sql.Migrations.Migrations
 
                     b.HasIndex(new[] { "CategoryName" }, "CategoryName");
 
-                    b.ToTable("Categories", (string)null);
+                    b.ToTable("Categories");
                 });
 
             modelBuilder.Entity("Northwind.Core.Models.Customer", b =>
@@ -137,7 +137,7 @@ namespace Northwind.Sql.Migrations.Migrations
 
                     b.HasIndex(new[] { "Region" }, "Region");
 
-                    b.ToTable("Customers", (string)null);
+                    b.ToTable("Customers");
                 });
 
             modelBuilder.Entity("Northwind.Core.Models.CustomerDemographic", b =>
@@ -155,7 +155,7 @@ namespace Northwind.Sql.Migrations.Migrations
 
                     SqlServerKeyBuilderExtensions.IsClustered(b.HasKey("CustomerTypeCode"), false);
 
-                    b.ToTable("CustomerTypes", (string)null);
+                    b.ToTable("CustomerTypes");
                 });
 
             modelBuilder.Entity("Northwind.Core.Models.Employee", b =>
@@ -241,7 +241,7 @@ namespace Northwind.Sql.Migrations.Migrations
                     b.HasIndex(new[] { "PostalCode" }, "PostalCode")
                         .HasDatabaseName("PostalCode1");
 
-                    b.ToTable("Employees", (string)null);
+                    b.ToTable("Employees");
                 });
 
             modelBuilder.Entity("Northwind.Core.Models.Invoice", b =>
@@ -429,7 +429,7 @@ namespace Northwind.Sql.Migrations.Migrations
 
                     b.HasIndex(new[] { "ShipVia" }, "ShippersOrders");
 
-                    b.ToTable("Orders", (string)null);
+                    b.ToTable("Orders");
                 });
 
             modelBuilder.Entity("Northwind.Core.Models.OrderDetail", b =>
@@ -464,7 +464,7 @@ namespace Northwind.Sql.Migrations.Migrations
 
                     b.HasIndex(new[] { "ProductId" }, "ProductsOrder_Details");
 
-                    b.ToTable("Order Details", (string)null);
+                    b.ToTable("Order Details");
                 });
 
             modelBuilder.Entity("Northwind.Core.Models.Product", b =>
@@ -528,7 +528,7 @@ namespace Northwind.Sql.Migrations.Migrations
 
                     b.HasIndex(new[] { "SupplierId" }, "SuppliersProducts");
 
-                    b.ToTable("Products", (string)null);
+                    b.ToTable("Products");
                 });
 
             modelBuilder.Entity("Northwind.Core.Models.Region", b =>
@@ -547,7 +547,7 @@ namespace Northwind.Sql.Migrations.Migrations
 
                     SqlServerKeyBuilderExtensions.IsClustered(b.HasKey("Id"), false);
 
-                    b.ToTable("Region", (string)null);
+                    b.ToTable("Region");
                 });
 
             modelBuilder.Entity("Northwind.Core.Models.Shipper", b =>
@@ -634,7 +634,7 @@ namespace Northwind.Sql.Migrations.Migrations
                     b.HasIndex(new[] { "PostalCode" }, "PostalCode")
                         .HasDatabaseName("PostalCode2");
 
-                    b.ToTable("Suppliers", (string)null);
+                    b.ToTable("Suppliers");
                 });
 
             modelBuilder.Entity("Northwind.Core.Models.Territory", b =>
@@ -660,7 +660,7 @@ namespace Northwind.Sql.Migrations.Migrations
 
                     b.HasIndex("RegionId");
 
-                    b.ToTable("Territories", (string)null);
+                    b.ToTable("Territories");
                 });
 
             modelBuilder.Entity("EmployeeTerritory", b =>
