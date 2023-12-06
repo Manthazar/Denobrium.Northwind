33154e175b7b25017d2269c18a5fe116f294cacd
Aron <arko@denobrium.net>
2023-12-06 13:07:18 +0100

    #13 replacing datagrid with telerik datagrid for much better performance

--- Frontend/UWP/Northwind.Backoffice/Pages/CustomerList/CustomerList.xaml.cs
+++ Frontend/UWP/Northwind.Backoffice/Pages/CustomerList/CustomerList.xaml.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace Northwind.Backoffice.Pages.CustomerList
 {
@@ -17,61 +16,6 @@ namespace Northwind.Backoffice.Pages.CustomerList
         {
             this.InitializeComponent();
             this.DataContext = new CustomerListViewModel();
-        }
-
-        /// <summary>
-        /// Manual implementation of sorting, since the UWP version of collection view source does not support sortdirections or filters :-/
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void CustomersGrid_Sorting(object sender, Microsoft.Toolkit.Uwp.UI.Controls.DataGridColumnEventArgs e)
-        {
-            var dg = (DataGrid)sender;
-            var sortTag = e.Column.Tag.ToString();
-
-            var sortDirection = DataGridSortDirection.Ascending;
-
-            if (e.Column.SortDirection != null && e.Column.SortDirection == DataGridSortDirection.Ascending)
-            {
-                // If the column is already sorted, flip the sort direction;
-                sortDirection = DataGridSortDirection.Descending;
-            }
-
-            //Use the Tag property to pass the bound column name for the sorting implementation
-            if (sortTag == "CustomerCode")
-            {
-                Sort((c) => c.CustomerCode, sortDirection, dg, e);
-            }
-            else if (sortTag == "CompanyName")
-            {
-                Sort((c) => c.CompanyName, sortDirection, dg, e);
-            }
-
-            foreach (var column in dg.Columns)
-            {
-                if (column.Tag?.ToString() != sortTag)
-                {
-                    column.SortDirection = null;
-                }
-            }
-        }
-
-        private static void Sort(Func<CustomerInfo, string> keySelector, DataGridSortDirection sortDirection, DataGrid dg, DataGridColumnEventArgs e)
-        {
-            var items = (IEnumerable<CustomerInfo>)dg.ItemsSource;
-
-            if (sortDirection == DataGridSortDirection.Ascending)
-            {
-                var sorted = items.OrderBy(keySelector);
-                dg.ItemsSource = new ObservableCollection<CustomerInfo>(sorted);
-            }
-            else
-            {
-                var sorted = items.OrderByDescending(keySelector);
-                dg.ItemsSource = new ObservableCollection<CustomerInfo>(sorted);
-            }
-
-            e.Column.SortDirection = sortDirection;
         }
     }
 }
--- Frontend/UWP/Northwind.Backoffice/Pages/CustomerList/CustomerListViewModel.cs
+++ Frontend/UWP/Northwind.Backoffice/Pages/CustomerList/CustomerListViewModel.cs
@@ -12,7 +12,9 @@ namespace Northwind.Backoffice.Pages.CustomerList
     {
         public CustomerListViewModel()
         {
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             LoadItemsAsync();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
         }
 
         private async Task LoadItemsAsync()
