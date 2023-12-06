9620a88a3f43e3be2ad2bedddcfb0823f758a695
Aron <arko@denobrium.net>
2023-12-06 12:32:56 +0100

    #13 Adding sorting implementation

--- Frontend/UWP/Northwind.Backoffice/Pages/CustomerList/CustomerList.xaml.cs
+++ Frontend/UWP/Northwind.Backoffice/Pages/CustomerList/CustomerList.xaml.cs
@@ -1,4 +1,9 @@
-﻿using Windows.UI.Xaml;
+﻿using Microsoft.Toolkit.Uwp.UI.Controls;
+using Northwind.BackOffice.Data;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
@@ -10,19 +15,67 @@ namespace Northwind.Backoffice.Pages.CustomerList
         public CustomerList()
         {
             this.InitializeComponent();
-            this.ViewModel = new CustomerListViewModel();
-            this.DataContext = ViewModel;
+            this.DataContext = new CustomerListViewModel();
         }
 
-        internal CustomerListViewModel ViewModel
+        /// <summary>
+        /// Manual implementation of sorting, since the UWP version of collection view source does not support sortdirections or filters :-/
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CustomersGrid_Sorting(object sender, Microsoft.Toolkit.Uwp.UI.Controls.DataGridColumnEventArgs e)
         {
-            get { return (CustomerListViewModel)GetValue(ViewModelProperty); }
-            set { SetValue(ViewModelProperty, value); }
+            var dg = (DataGrid)sender;
+            var sortTag = e.Column.Tag.ToString();
+            var items = (IEnumerable<CustomerInfo>)dg.ItemsSource;
+
+            var sortDirection = DataGridSortDirection.Ascending;
+
+            if (e.Column.SortDirection != null && e.Column.SortDirection == DataGridSortDirection.Ascending)
+            {
+                // If the column is already sorted, flip the sort direction;
+                sortDirection = DataGridSortDirection.Descending;
+            }
+
+            //Use the Tag property to pass the bound column name for the sorting implementation
+            if (sortTag == "CustomerCode")
+            {
+                if (sortDirection == DataGridSortDirection.Ascending)
+                {
+                    var sorted = items.OrderBy(ci => ci.CustomerCode);
+                    dg.ItemsSource = new ObservableCollection<CustomerInfo>(sorted);
+                }
+                else
+                {
+                    var sorted = items.OrderByDescending(ci => ci.CustomerCode);
+                    dg.ItemsSource = new ObservableCollection<CustomerInfo>(sorted);
+                }
+
+                e.Column.SortDirection = sortDirection;
+            }
+            else if (sortTag == "CompanyName")
+            {
+                if (sortDirection == DataGridSortDirection.Ascending)
+                {
+                    var sorted = items.OrderBy(ci => ci.CompanyName);
+                    dg.ItemsSource = new ObservableCollection<CustomerInfo>(sorted);
+                }
+                else
+                {
+                    var sorted = items.OrderByDescending(ci => ci.CompanyName);
+                    dg.ItemsSource = new ObservableCollection<CustomerInfo>(sorted);
+                }
+
+                e.Column.SortDirection = sortDirection;
+            }
+
+            foreach (var column in dg.Columns)
+            {
+                if (column.Tag?.ToString() != sortTag)
+                {
+                    column.SortDirection = null;
+                }
+            }
         }
-
-        private static readonly DependencyProperty ViewModelProperty =
-            DependencyProperty.Register("ViewModel", typeof(CustomerListViewModel), typeof(CustomerList), new PropertyMetadata(0));
-
-
     }
 }
